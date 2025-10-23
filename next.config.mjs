bbe327916f9e3aea855b7ccf0eba6c22953926e8
Robert <yutuan.victory@gmail.com>
2025-10-23 10:44:57 +0800

    chore: remove unused search API route

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: "export",
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
